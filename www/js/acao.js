80ee8129129b652e679f382e262eab017e2c2cfb
Guhh235 <g.silva2503tavares@gmail.com>
2024-08-24 10:27:35 +0900

    Project's Initial Commit

---
+++ www/js/acao.js
@@ -0,0 +1,13 @@
+
+
+$(document).on("click","#somar",function(){
+    var valor1 = $("#valor1").val();
+    var valor2 = $("#valor2").val();
+
+    var resultado = parseFloat(valor1) + parseFloat(valor2);
+
+    $("#resultado").val(resultado);
+
+    });
+    
+       
